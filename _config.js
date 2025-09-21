8fc025871f494143a40fccf2307718db2821102c
machoyamwangi <machoya.mwangi@gmail.com>
2025-09-21 11:55:01 +0300

    updated credentials/ added dotenv

--- _config.js
+++ _config.js
@@ -1,10 +1,11 @@
 var config = {}
+require('dotenv').config();  // load .env variables
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://machoyamwangi_db_user:Gorjayas2010@cluster0.2jsyouj.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0',
-    development: 'mongodb+srv://machoyamwangi_db_user:Gorjayas2010@cluster0.2jsyouj.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Cluster0',
-    test: 'mongodb+srv://machoyamwangi_db_user:Gorjayas2010@cluster0.2jsyouj.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Cluster0',
+    production: 'mongodb+srv://<mongouser>:<mongopwd>@cluster0.2jsyouj.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://<mongouser>:<mongopwd>@cluster0.2jsyouj.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://<mongouser>:<mongopwd>@cluster0.2jsyouj.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Cluster0',
 }
 module.exports = config;
 
--- server.js
+++ server.js
@@ -1,53 +1,38 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
 const app = express();
 
-// Define routes
+// Routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
-// connecting the database
-//let mongodb_url = 'mongodb://localhost:27017/';
-//let dbName = 'darkroom';
-//mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-//    if (err) console.log(err)
-//});
+// MongoDB connection
+const MONGO_URI = process.env.MONGO_URI;
 
-
-
-//const MONGO_URI = `mongodb+srv://machoyamwangi_db_user:Gorjayas2010@ cluster0.2jsyouj.mongodb.net-dev/?retryWrites=true&w=majority`;
-
-const MONGO_URI = 'mongodb+srv://machoyamwangi_db_user:Gorjayas2010@cluster0.2jsyouj.mongodb.net/gallery?retryWrites=true&w=majority';
-
-
-
-// test if the database has connected successfully
-
-// Connect to MongoDB using Mongoose
-mongoose.connect(MONGO_URI)
-  .then(() => console.log('Database connected successfully!!'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('✅ Database connected successfully!!'))
+  .catch(err => console.error('❌ MongoDB connection error:', err));
 
 // View Engine
 app.set('view engine', 'ejs');
 
-// Set up the public folder;
+// Public folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// body parser middleware
-app.use(express.json())
-
+// JSON parser middleware
+app.use(express.json());
 
 app.use('/', index);
 app.use('/image', image);
 
-
-
- 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,"0.0.0.0",() =>{
-    console.log(`Server is listening at port ${PORT}`)
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server is listening at port ${PORT}`);
 });
