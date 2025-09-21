65f73d00832c3f27b9b7fc413a55f3926994233b
machoyamwangi <machoya.mwangi@gmail.com>
2025-09-21 10:28:21 +0300

    updated port

--- server.js
+++ server.js
@@ -2,26 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const app = express();
 
 // Define routes
 let index = require('./routes/index');
 let image = require('./routes/image');
 
 // connecting the database
-let mongodb_url = 'mongodb://localhost:27017/';
-let dbName = 'darkroom';
-mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
-    if (err) console.log(err)
-});
+//let mongodb_url = 'mongodb://localhost:27017/';
+//let dbName = 'darkroom';
+//mongoose.connect(`${mongodb_url}${dbName}`,{ useNewUrlParser: true , useUnifiedTopology: true }, (err)=>{
+//    if (err) console.log(err)
+//});
+
+
+
+const MONGO_URI = `mongodb+srv://machoyamwangi_db_user:Gorjayas2010@cluster0.2jsyouj.mongodb.net/darkroom-dev?retryWrites=true&w=majority`;
+
 
 // test if the database has connected successfully
-let db = mongoose.connection;
-db.once('open', ()=>{
-    console.log('Database connected successfully')
-})
 
-// Initializing the app
-const app = express();
+// Connect to MongoDB using Mongoose
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Database connected successfully!!'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 
 // View Engine
@@ -41,6 +45,6 @@ app.use('/image', image);
 
  
 const PORT = process.env.PORT || 5000;
-app.listen(PORT,() =>{
-    console.log(`Server is listening at http://localhost:${PORT}`)
+app.listen(PORT,"0.0.0.0",() =>{
+    console.log(`Server is listening at port ${PORT}`)
 });
