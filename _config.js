7fd6b592dc70a55514927700577ee285cd63bc9a
machoyamwangi <machoya.mwangi@gmail.com>
2025-09-23 10:50:40 +0300

    updated changes to .env & jenkins file

--- _config.js
+++ _config.js
@@ -1,9 +1,13 @@
-var config = {}
+require('dotenv').config({ quiet: true, inject: {} });
 
-// Update to have your correct username and password
+var config = {};
+
+const { MONGO_USERNAME: username, MONGO_PASSWORD: password} = process.env;
+
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
-}
+    production: `mongodb+srv://${username}:${password}@cluster0.2jsyouj.mongodb.net/darkroom?retryWrites=true&w=majority&appName=Cluster0`,
+    development: `mongodb+srv://${username}:${password}@cluster0.2jsyouj.mongodb.net/darkroom-dev?retryWrites=true&w=majority&appName=Cluster0`,
+    test: `mongodb+srv://${username}:${password}@cluster0.2jsyouj.mongodb.net/darkroom-test?retryWrites=true&w=majority&appName=Cluster0`,
+
+    }
 module.exports = config;
